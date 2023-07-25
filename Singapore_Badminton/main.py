37d63ad1eb07cc57bc1d9f07a77d81cd1c7e789b
Thomas <tomatheickal@hotmail.com>
2023-07-25 23:54:25 +0100

    Fixed bug for end of month days. Added dry-run

--- Singapore_Badminton/main.py
+++ Singapore_Badminton/main.py
@@ -28,12 +28,13 @@ LOGIN_SUBMIT_BUTTON_NAME = "Submit1"
 BOOKING_FACILITY_LINK = "Booking of Facility"
 BADMINTON_BKG_LINK = "BADMINTON BKG"
 
+DATE_PICKER_NAME = "dday"
 BOOK_NOW_BUTTON_ID = "submit1"
 
 EMAIL_INPUT_ID = "txtEmail"
 
 logger.remove(0)
-logger.add(sys.stderr, level="INFO")
+logger.add(sys.stderr, level="DEBUG")
 
 
 def argparser():
@@ -44,8 +45,9 @@ def argparser():
     parser.add_argument(
         "--email", type=str, help="The email required for the booking confirmation"
     )
+    parser.add_argument("-d", "--dry", action="store_true", help="Dry-Run")
     args = parser.parse_args()
-    return args.start_time, args.email
+    return args.start_time, args.email, args.dry
 
 
 def login():
@@ -66,18 +68,33 @@ def navigateToBookingPage():
     browserComponents.waitForElementToBeVisible(By.LINK_TEXT, BADMINTON_BKG_LINK, 3)
     browserComponents.findElementAndClick(By.LINK_TEXT, BADMINTON_BKG_LINK)
 
-    localcurrentdateandtime = datetime.now()
-    currentDay = localcurrentdateandtime.strftime("%d")
-    nextWeekDate = int(currentDay) + 7
-    logger.debug(f"Next week date : {nextWeekDate}")
+    singapore_currentdateandtime = datetime.now() + timedelta(hours=7)
+    currentDay = singapore_currentdateandtime.strftime("%-d")
+    logger.debug(f"Today's date in Singapore: {currentDay}")
 
-    browserComponents.waitForElementToBeVisible(By.LINK_TEXT, str(nextWeekDate), 2)
-    browserComponents.findElementAndClick(By.LINK_TEXT, str(nextWeekDate))
+    browserComponents.waitForElementToBeVisible(By.LINK_TEXT, str(currentDay), 2)
+    browserComponents.findElementAndClick(By.LINK_TEXT, str(currentDay))
 
 
-def choose_court(start_time):
-    logger.info("Choosing Court")
+def choose_date_and_court(start_time):
+    logger.info("Choosing date from dropdown")
     browserComponents.waitForElementToBeVisible(By.ID, BOOK_NOW_BUTTON_ID, 2)
+    browserComponents.findElementAndClick(By.NAME, DATE_PICKER_NAME)
+    sixDaysDate = datetime.now() + timedelta(days=6)
+    nextWeekDate = datetime.now() + timedelta(weeks=1)
+
+    sixDaysDateFormatted = sixDaysDate.strftime("%-d %b %Y")
+    nextWeekDateFormatted = nextWeekDate.strftime("%-d %b %Y")
+
+    logger.debug(f"6 days ahead: {sixDaysDateFormatted}")
+    logger.debug(f"Next week: {nextWeekDateFormatted}")
+    logger.info(f"Clicking date: {nextWeekDateFormatted}")
+    browserComponents.findElementByAttributeAndClick(
+        "option", "value", nextWeekDateFormatted
+    )
+    browserComponents.waitForElementToBeVisible(By.ID, BOOK_NOW_BUTTON_ID, 2)
+
+    logger.info("Choosing Court")
     courtElements7am = browserComponents.getElementsInRow("Table3", start_time - 6)
     preference_order = (4, 5, 3, 1, 2, 6)
     for court in preference_order:
@@ -92,27 +109,28 @@ def choose_court(start_time):
 
 
 def choose_time_and_submit(start_time: int, court: int):
-    todayDate = datetime.now() + timedelta(weeks=1)
-    todayDateFormatted = todayDate.strftime("%d %b %Y")
-    BOOKING_TABLE_CHECKBOX_VALUE = f"{str(start_time).zfill(2)}:00,{str(start_time+1).zfill(2)}:00,BC{str(court)},{todayDateFormatted},B"
+    newtWeekDate = datetime.now() + timedelta(weeks=1)
+    nextWeekFormatted = newtWeekDate.strftime("%-d %b %Y")
+    BOOKING_TABLE_CHECKBOX_VALUE = f"{str(start_time).zfill(2)}:00,{str(start_time+1).zfill(2)}:00,BC{str(court)},{nextWeekFormatted},B"
 
-    browserComponents.findInputByAttributeAndClick(
-        "value", BOOKING_TABLE_CHECKBOX_VALUE
+    browserComponents.findElementByAttributeAndClick(
+        "input", "value", BOOKING_TABLE_CHECKBOX_VALUE
     )
 
     logger.debug(f"Clicking Checkbox: {BOOKING_TABLE_CHECKBOX_VALUE}")
     browserComponents.findElementAndClick(By.ID, BOOK_NOW_BUTTON_ID)
 
 
-def enter_email_and_book(email):
+def enter_email_and_book(email, dry_run):
     logger.debug(f"Sending email: {email} and booking")
     browserComponents.waitForElementToBeVisible(By.ID, EMAIL_INPUT_ID, 5)
     browserComponents.findInputAndSendKeys(By.ID, EMAIL_INPUT_ID, email)
-    # browserComponents.findElementAndClick(By.ID, BOOK_NOW_BUTTON_ID)
+    if not dry_run:
+        browserComponents.findElementAndClick(By.ID, BOOK_NOW_BUTTON_ID)
 
 
 if __name__ == "__main__":
-    start_time, email = argparser()
+    start_time, email, dry_run = argparser()
 
     driver = webdriver.Chrome()
     browserComponents = BrowserComponents(driver)
@@ -120,8 +138,8 @@ if __name__ == "__main__":
     driver.get("https://www.sswimclub.org.sg/MembersWeb/main/loginuser.asp")
     login()
     navigateToBookingPage()
-    court = choose_court(start_time)
+    court = choose_date_and_court(start_time)
     choose_time_and_submit(start_time, court)
-    enter_email_and_book(email)
+    enter_email_and_book(email, dry_run)
 
     time.sleep(2)
