1827f1679053d6e8793562bc658a13919e503bf1
Thomas <tomatheickal@hotmail.com>
2023-09-30 16:05:13 +0100

    Added sleep for 45 seconds

--- Singapore_Badminton/main.py
+++ Singapore_Badminton/main.py
@@ -90,6 +90,7 @@ def choose_date_and_court(start_time):
 
     logger.info(f" Attempting to click date: {eightDaysDate}")
     wait_time = 0
+    time.sleep(45)
     while datetime.today().strftime("%-d %b %Y") != tomorrowDate:
         if wait_time == 120:
             logger.info("Date not found after 2 minutes")
