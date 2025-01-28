0bed80723c051d7417fd1031bd3ea5a2145bd5c3
Thomas <tomatheickal@hotmail.com>
2025-01-28 22:48:24 +0000

    enable headless mode

--- Singapore_Badminton/src/main.py
+++ Singapore_Badminton/src/main.py
@@ -230,9 +230,9 @@ if __name__ == "__main__":
     start_time, dry_run, skip_next_day = argparser()
 
     options = Options()
-    # options.add_argument("--headless")
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=options
